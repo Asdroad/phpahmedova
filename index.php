5ce5d3aec318764fb0fc79fb09506f017d0b0006
griseldy <khairov.tima@bk.ru>
2024-04-21 18:35:27 +0300

    добавление фотографии товара и футтера и легкий редакт

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 <body>
 <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
     <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none me-3">
-        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94" role="img"><title>Sooststrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>
         <span class="fs-4">SilentSonics</span>
     </a>
 
@@ -33,6 +32,7 @@
                     <h4 class="my-0 fw-normal">MacBook Pro (13 дюймов, M2, 2022 г.)</h4>
                 </div>
                 <div class="card-body">
+                    <img src="img/macbook_pro_13.jpg" class="card-img-top" alt="MacBook Pro 13" style="max-height: 400px;">
                     <h1 class="card-title pricing-card-title">500₽<small class="text-body-secondary fw-light">/день</small></h1>
                     <ul class="list-unstyled mt-3 mb-4">
                         <li>Apple M2 chip</li>
@@ -52,12 +52,11 @@
                     <h4 class="my-0 fw-normal">MacBook Pro (14 дюймов, ноябрь 2023 г.)</h4>
                 </div>
                 <div class="card-body">
+                    <img src="img/macbook_pro_14.jpg" class="card-img-top" alt="MacBook Pro 13" style="max-height: 400px;" >
                     <h1 class="card-title pricing-card-title">750₽<small class="text-body-secondary fw-light">/день</small></h1>
                     <ul class="list-unstyled mt-3 mb-4">
                         <li>Apple M3 chip</li>
                         <li>8-ядерный процессор, 4 производительных ядра и 4 энергоэффективных ядра</li>
-
-
                         <li>10-core GPU</li>
                         <li>16GB оперативной памяти</li>
                         <li>256GB SSD</li>
@@ -75,6 +74,7 @@
                     <h4 class="my-0 fw-normal">MacBook Pro (16 дюймов, ноябрь 2023 г.)</h4>
                 </div>
                 <div class="card-body">
+                    <img src="img/macbook_pro_16.jpg" class="card-img-top" alt="MacBook Pro 13" style="max-height: 400px;" >
                     <h1 class="card-title pricing-card-title">1000₽<small class="text-body-secondary fw-light">/день</small></h1>
                     <ul class="list-unstyled mt-3 mb-4">
                         <li>Apple M3 Pro chip</li>
@@ -94,6 +94,7 @@
                     <h4 class="my-0 fw-normal">MacBook Air (13 дюймов, ноябрь 2020 г.)</h4>
                 </div>
                 <div class="card-body">
+                    <img src="img/macbook_air_13.jpg" class="card-img-top" alt="MacBook Pro 13" style="max-height: 400px;" >
                     <h1 class="card-title pricing-card-title">350₽<small class="text-body-secondary fw-light">/день</small></h1>
                     <ul class="list-unstyled mt-3 mb-4">
                         <li>Apple M1 chip</li>
@@ -108,6 +109,39 @@
             </div>
         </div>
     </div>
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+            <div class="col-6 col-md offset-md-2">
+                <h5>Features</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Resources</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>About</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </main>
 </body>
 </html>
